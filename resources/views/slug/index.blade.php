296cdfd70b243508ef4c54d753cb26de3aa818f6
MikeHopley <mike.hopley@gmail.com>
2023-08-30 14:33:37 +0100

    Remove Slug surrender info

--- resources/views/slug/index.blade.php
+++ resources/views/slug/index.blade.php
@@ -5,8 +5,6 @@
 	:groupLayout="true"
 >
 
-<p>Slug surrenders are never worth taking, except to unlock the Slug Cruiser. That does not apply to pirates.</p>
-
 <p>All Slug ships have Slug Repair Gel, including pirates.</p>
 
 <x-slot name="explosives">
